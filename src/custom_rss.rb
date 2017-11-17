4ad02121930d50ad1dd1254eb944f3eddfe92318
sugarnaoming <sakigithub@gmail.com>
2017-11-17 14:03:08 +0900

    rss取得用独自クラスファイルを追加

---
+++ src/custom_rss.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'rss'
+
+class Rss
+  # 引数に渡されたurlに対してRSSの取得を試みます
+  # @params [String] url: RSSを取得するURL
+  # @params [Boolean] fake_agent: ユーザエージェントを偽装します
+  # @return [RSS::Feed] rssのfeed形式で返します
+  def get_rss(url:, fake_agent: false)
+    opt = {}
+    opt = {'User-Agent' => 'Opera/9.80 (Windows NT 5.1)'} if fake_agent
+    open(url, opt){ |rss| RSS::Parser.parse(rss) }
+  end
+end
